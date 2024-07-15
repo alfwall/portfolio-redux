781f67f63aa250282171dadeac9afb54b04af836
Alfred Wallace <contact@alfredwallace.me>
2024-07-15 14:14:03 -0700

    Styling fixes

--- src/App/AboutMe/index.jsx
+++ src/App/AboutMe/index.jsx
@@ -1,20 +1,24 @@
 import Header from "../Header"
 import Footer from "../Footer"
-
+import "./index.css"
 
 function AboutMe() {
   return (
     <>
       <Header selected="about-me" />
-      <div className="col-sm-6 col-xs-12">
-        <img src="../selfie.jpg" alt="A photo of Alfred." />
-      </div>
-      <div className="col-sm-6 col-xs-12">
-        <h2>About Me</h2>
-        <p>
-          Alfred is an established software engineer with 8 years of experience
-          building applications for web and desktop platforms.
-        </p>
+      <div id="about-me" className="col-xs-12">
+        <div className="row">
+          <div className="img-div col-sm-6 col-xs-12">
+            <img src="../selfie.jpg" alt="A photo of Alfred." />
+          </div>
+          <div className="about-me-text col-sm-6 col-xs-12">
+            <h2>About Me</h2>
+            <p>
+              Alfred is an established software engineer with 8 years of experience
+              building applications for web and desktop platforms.
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
--- src/App/Resume/index.jsx
+++ src/App/Resume/index.jsx
@@ -1,14 +1,17 @@
 import Header from "../Header"
 import Footer from "../Footer"
+import "./index.css"
 
 function Resume() {
     return (
         <>
             <Header selected="resume" />
-            {/* Put downloadable file here */}
-            <a href="">
-                <img src="" alt="A download symbol" />
-            </a>
+            <div id="resume-div">
+                <h2>My Resume</h2>
+                <a href="../../AlfredWallace_SoftwareEngineer.pdf">
+                    <img src="../../download.png" alt="A download symbol" />
+                </a>
+            </div>
             <Footer />
         </>
     );
